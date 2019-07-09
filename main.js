94e84704058bd5a112d5d6fff8128932424fe558
Urmeli12321 <40326025+Urmeli12321@users.noreply.github.com>
2019-07-09 16:30:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -11,9 +11,9 @@ const BotSettings = require("./botsettings.json")
 
     console.log(`\nBot ist online.\nName + Tag: ${bot.user.username}#${bot.user.discriminator}\nPrefix: ${BotSettings.prefix}`)
     bot.user.setStatus("dnd")//online, idle, dnd, invisible
-    bot.user.setActivity(`${BotSettings.prefix}help mit maggi`, {
+    bot.user.setActivity(`${BotSettings.prefix}Alexi-chan ❤`, {
 
-        type: "PLAYING" //PLAYING, STREAMING, LISTENING, WATCHING
+        type: "WATCHING" //PLAYING, STREAMING, LISTENING, WATCHING
     })
     //Name + Avatar
     // bot.user.setUsername("Bendy")
